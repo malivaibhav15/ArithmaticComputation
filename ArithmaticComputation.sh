c08390edb7c0a18068be7600b54b90f61c13b0ae
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-02-27 16:56:40 +0530

    ADDED : Store the result in dictionary

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -3,6 +3,7 @@ operation1=1
 operation2=1
 operation3=1
 operation4=1
+declare -A result
 read -p "Enter first number=" number1
 read -p "Enter second number=" number2
 read -p "Enter third number=" number3
@@ -14,4 +15,8 @@ operation3=`expr "(($number3+$number1))/$number2" | bc -l`
 echo "Result of opeation (c+a/b)=$operation3"
 operation4=$(( $(($number1%$number2))+$number3 ))
 echo "Result of opeation (a%b+c)=$operation4"
-
+result[operation1]=$operation1
+result[operation2]=$operation2
+result[operation3]=$operation3
+result[operation4]=$operation4
+echo "Dictionary=${result[@]}"
