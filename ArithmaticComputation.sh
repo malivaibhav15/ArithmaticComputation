855b92644464b39794230fb7b83c4ba043c67063
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-02-27 15:46:36 +0530

    ADDED : Performs operation a*b+c

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -x
-result=1
+operation1=1
+operation2=1
 read -p "Enter first number=" number1
 read -p "Enter second number=" number2
 read -p "Enter third number=" number3
-result=$(( $(($number1+$number2))*$number3 ))
-echo "Result of opeation (a+b*c)=$result"
+operation1=$(( $(($number1+$number2))*$number3 ))
+echo "Result of opeation (a+b*c)=$operation1"
+operation2=$(( $(($number1*$number2))+$number3 ))
+echo "Result of opeation (a*b+c)=$operation2"
+
