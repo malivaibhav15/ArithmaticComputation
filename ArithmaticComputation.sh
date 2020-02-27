64b5d2a4c11bfe16c0b2f56b12204e7ac1b535ab
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-02-27 17:38:02 +0530

    ADDED : Convert dictionary to array

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -4,6 +4,7 @@ operation2=1
 operation3=1
 operation4=1
 declare -A result
+declare -a array
 read -p "Enter first number=" number1
 read -p "Enter second number=" number2
 read -p "Enter third number=" number3
@@ -15,8 +16,14 @@ operation3=`expr "(($number3+$number1))/$number2" | bc -l`
 echo "Result of opeation (c+a/b)=$operation3"
 operation4=$(( $(($number1%$number2))+$number3 ))
 echo "Result of opeation (a%b+c)=$operation4"
-result[operation1]=$operation1
-result[operation2]=$operation2
-result[operation3]=$operation3
-result[operation4]=$operation4
+result[0]=$operation1
+result[1]=$operation2
+result[2]=$operation3
+result[3]=$operation4
 echo "Dictionary=${result[@]}"
+size=${#result[@]}
+for (( counter=0; counter<$size; counter++ ))
+do
+	array[counter]=${result[$counter]}
+done
+echo "Array={${array[@]}}"
