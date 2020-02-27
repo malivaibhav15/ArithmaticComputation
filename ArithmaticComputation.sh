ea4155b24e5fd77bd573fd8f2685a0c1249613de
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-02-27 16:29:10 +0530

    ADDED : Performs operation a%b+c

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -2,6 +2,7 @@
 operation1=1
 operation2=1
 operation3=1
+operation4=1
 read -p "Enter first number=" number1
 read -p "Enter second number=" number2
 read -p "Enter third number=" number3
@@ -11,4 +12,6 @@ operation2=$(( $(($number1*$number2))+$number3 ))
 echo "Result of opeation (a*b+c)=$operation2"
 operation3=`expr "(($number3+$number1))/$number2" | bc -l`
 echo "Result of opeation (c+a/b)=$operation3"
+operation4=$(( $(($number1%$number2))+$number3 ))
+echo "Result of opeation (a%b+c)=$operation4"
 
